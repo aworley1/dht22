f2546e22771ac40cbc0bb479edfc370035ae683b
Andrew Worley <andrew.worley@johnlewis.co.uk>
2021-01-14 21:18:56 +0000

    Add a listener

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,7 @@
 import com.pi4j.io.gpio.GpioFactory
 import com.pi4j.io.gpio.RaspiPin
+import com.pi4j.io.gpio.event.GpioPinListener
+import com.pi4j.io.gpio.event.GpioPinListenerDigital
 import com.pi4j.wiringpi.GpioUtil
 
 val DHT_22 = RaspiPin.GPIO_04
@@ -17,7 +19,11 @@ fun main() {
 
     val input = gpio.provisionDigitalInputPin(DHT_22)
 
-    while (true){
-        println(input.state)
-    }
+    input.addListener(
+        GpioPinListenerDigital {
+            println("${System.nanoTime()}: ${it.state}")
+        }
+    )
+
+    while (true) {}
 }
